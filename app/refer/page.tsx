5f9757763562b36934bc0917286bc0de0081f05a
= <=>
2024-05-27 20:46:21 +0600

    Build the refer page

--- app/refer/page.tsx
+++ app/refer/page.tsx
@@ -1,6 +1,13 @@
 import Footer from "@/components/Footer";
 import { Grid } from "@radix-ui/themes";
 import Image from "next/image";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import { Button } from "@/components/ui/button";
 
 const Refer = () => {
   return (
@@ -20,7 +27,7 @@ const Refer = () => {
           coins too every time!
         </p>
 
-        <div className="p-5 bg-[#F68456] w-[280px] space-y-3 rounded-3xl">
+        <div className="p-4 bg-[#F68456] w-[280px] space-y-3 rounded-3xl border-dashed border-2 border-white">
           <div className="flex items-end justify-between">
             <p className="text-[12px]">Your referral code </p>
             <button className="bg-primary px-3 py-2 text-sm rounded-3xl">
@@ -36,10 +43,49 @@ const Refer = () => {
         </div>
         <p>Share your Referral code via</p>
       </div>
-      <div className="overflow-auto pb-2"></div>
+      <div className="overflow-auto pb-2 py-4 px-3">
+        <div className="bg-white p-4 text-sm border rounded-3xl">
+          <p className="text-center font-semibold">
+            Were you referred by your friend?
+          </p>
+          <form className="w-full my-3 flex flex-col">
+            <label htmlFor="i">Enter referral code</label>
+            <input type="text" className="outline-none border-b" />
+          </form>
+          <div className="flex items-center gap-4">
+            <Button>Verify</Button>
+            <Button variant="secondary">Redeem 50 coins</Button>
+          </div>
+        </div>
+        <div className="flex items-center justify-between py-8">
+          <Button className="bg-green-500 shadow-md">
+            <FaWhatsapp className="text-xl mr-1" /> Invite Via WhatsApp
+          </Button>
+          <Button variant="secondary" className="shadow-md">
+            <IoShareSocialOutline className="text-xl mr-1" /> Other Options
+          </Button>
+        </div>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>How it work?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      </div>
       <Footer />
     </Grid>
   );
 };
 
 export default Refer;
+
+import { FaWhatsapp } from "react-icons/fa6";
+import { IoShareSocialOutline } from "react-icons/io5";
